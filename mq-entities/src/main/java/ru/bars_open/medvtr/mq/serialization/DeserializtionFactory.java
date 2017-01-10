8c8dca19f96a4b78d1276e9953dbd0f96b01d3dc
Upatov Egor <EUpatov@korusconsulting.ru>
2017-01-10 14:40:09 +0300

    merge

---
+++ mq-entities/src/main/java/ru/bars_open/medvtr/mq/serialization/DeserializtionFactory.java
@@ -0,0 +1,9 @@
+package ru.bars_open.medvtr.mq.serialization;
+
+/**
+ * Author: Upatov Egor <br>
+ * Date: 22.12.2016, 16:46 <br>
+ * Company: Bars Group [ www.bars.open.ru ]
+ * Description:
+ */
+public class DeserializtionFactory {}
